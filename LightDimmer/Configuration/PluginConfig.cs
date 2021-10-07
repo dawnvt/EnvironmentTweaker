0919440249aebfedd292c795fd664f685496dd64
dawnvt <64136899+dawnvt@users.noreply.github.com>
2021-10-07 14:51:58 +0200

    Add project files

---
+++ LightDimmer/Configuration/PluginConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace ScoreRequirement.Configuration
+{
+    public class PluginConfig
+    {
+        
+    }
+}
---
+++ LightDimmer/Plugin.cs
@@ -0,0 +1,30 @@
+﻿using Hive.Versioning;
+using IPA;
+using SiraUtil.Zenject;
+using Zenject;
+using IPALogger = IPA.Logging.Logger;
+using IPA.Config.Stores;
+using IPA.Loader;
+using JetBrains.Annotations;
+using ScoreRequirement.Configuration;
+using ScoreRequirement.Installers;
+using Config = IPA.Config.Config;
+
+namespace ScoreRequirement
+{
+    [Plugin(RuntimeOptions.DynamicInit)]
+    public class Plugin
+    {
+        private PluginConfig _config;
+
+        [Init]
+        public void Init(Zenjector zenjector, Config config, IPALogger logger, PluginMetadata pluginMetadata)
+        {
+        }
+
+        [OnEnable, OnDisable]
+        public void OnStateChanged()
+        {
+        }
+    }
+}
