f647ea6967e1ebed0c5e7687a182dea4ed6e4372
dawnvt <64136899+dawnvt@users.noreply.github.com>
2021-10-08 11:07:10 +0200

    Made viewcontroller

--- LightDimmer/UI/ViewControllers/ModSettingsViewController.cs
+++ LightDimmer/UI/ViewControllers/ModSettingsViewController.cs
@@ -1,9 +1,24 @@
-﻿using HMUI;
+﻿using System;
+using System.ComponentModel;
+using HMUI;
+using BeatSaberMarkupLanguage;
+using BeatSaberMarkupLanguage.Attributes;
+using BeatSaberMarkupLanguage.Components;
+using BeatSaberMarkupLanguage.ViewControllers;
+using Zenject;
 
 namespace LightDimmer.UI.ViewControllers
 {
-    public class ModSettingsViewController : ViewController
+    [ViewDefinition("LightDimmer.UI.ModSettingsView.bsml")]
+    [HotReload(RelativePathToLayout = @"..\UI\ModSettingsView.bsml")]
+    public class ModSettingsViewController : IInitializable, IDisposable
     {
-        
+        public void Initialize()
+        {
+        }
+
+        public void Dispose()
+        {
+        }
     }
 }
