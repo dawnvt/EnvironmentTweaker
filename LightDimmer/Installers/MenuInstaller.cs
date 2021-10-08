e696ef45eb85ce15c13bb202f8e1d4533804b33a
dawnvt <64136899+dawnvt@users.noreply.github.com>
2021-10-08 11:06:56 +0200

    Installing things as it should

--- LightDimmer/Installers/MenuInstaller.cs
+++ LightDimmer/Installers/MenuInstaller.cs
@@ -1,5 +1,8 @@
-﻿using LightDimmer.Configuration;
+﻿using IPA.Logging;
+using LightDimmer.Configuration;
 using LightDimmer.UI.ViewControllers;
+using SiraUtil;
+using SiraUtil.Tools;
 using Zenject;
 
 namespace LightDimmer.Installers
@@ -7,15 +10,18 @@ namespace LightDimmer.Installers
     public class MenuInstaller : Installer
     {
         private readonly PluginConfig _config;
+        private readonly Logger _logger;
         
-        public MenuInstaller(PluginConfig config)
+        public MenuInstaller(PluginConfig config, Logger logger)
         {
             _config = config;
+            _logger = logger;
         }
         
         public override void InstallBindings()
         {
             Container.BindInstance(_config).AsSingle();
+            Container.BindLoggerAsSiraLogger(_logger);
             Container.BindInterfacesAndSelfTo<ModSettingsViewController>().AsSingle();
         }
     }
